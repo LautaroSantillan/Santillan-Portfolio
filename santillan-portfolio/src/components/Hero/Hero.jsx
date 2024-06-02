f8b1d85188c79ddac7854c6169a8a0f0386c34a1
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-06-01 23:53:13 -0300

    Version 11 - added CV

--- santillan-portfolio/src/components/Hero/Hero.jsx
+++ santillan-portfolio/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "./Hero.module.css";
 import { getImageUrl } from '../../utils';
+import CV from '../../download/Curriculum-Vitae-English--Santillan-Lautaro.pdf';
 
 export const Hero = () => {
     return (
@@ -10,7 +11,10 @@ export const Hero = () => {
                     I'm full-stack developer Jr, with 2 years of experience.
                     I'm always learning and practice about differents programming languages.  
                 </p>
-                <a href='https://www.linkedin.com/in/lautaro-ezequiel-santillan-65973b205/' className={styles.contactBtn} target="blank">Contact Me</a>
+                <div className={styles.containerBtn}>
+                    <a href='https://www.linkedin.com/in/lautaro-ezequiel-santillan-65973b205/' className={styles.contactBtn} target="blank">Contact Me</a>
+                    <a href={CV} download className={styles.contactBtn}>Download my CV</a>
+                </div>
             </div>
             <img src={getImageUrl("hero/fotoLautaro.png")} alt='photoMe' className={styles.meImg}/>
             <div className={styles.topBlur} />
